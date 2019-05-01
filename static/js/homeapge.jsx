1f2209f4cd8dc7dcfa63e5af23a2c77bf086d307
Ashley Ma <ashleyheidima@gmail.com>
2019-05-01 16:05:46 -0700

    inital commit

---
+++ static/js/homeapge.jsx
@@ -0,0 +1,25 @@
+class Homepage extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        
+      };
+    }
+  
+  
+    render() {
+      return (
+        <div className="container">
+              
+
+          
+        </div> 
+      );
+    }
+  }
+  
+  
+ReactDOM.render(
+  <Homepage />,
+  document.getElementById('root')
+);
